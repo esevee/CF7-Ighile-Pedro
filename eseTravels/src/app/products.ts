6727fd6a98eff4c25bb3cc2c0770bc55d0019f20
Ighile Pedro <esevee@yahoo.com>
2020-10-25 18:39:55 +0100

    new

--- eseTravels/src/app/products.ts
+++ eseTravels/src/app/products.ts
@@ -1,20 +1,26 @@
 export const products = [
     {
         place: "Angola",
-        Price:  3000,
+        date: "27.11.2020",
+        duration: "Two weeks",
+        price:  3000,
         description: "Breakfast and Safari"
 
     },
     {
         place: "Cape verde",
-        Price:  5000,
-        description: "Breakfast and Safari"
+        date: "27.11.2020",
+        duration: "Nine Days",
+        price:  4000,
+        description: "Breakfast and Hic, repellendus? Safari"
 
     },
     {
         place: "Durban",
-        Price:  4000,
-        description: "Breakfast and Safari"
+        date: "27.11.2020",
+        duration: "Ten Days",
+        price:  5000,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
 
     }
 ];
